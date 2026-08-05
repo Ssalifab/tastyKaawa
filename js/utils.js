a8f9d623896a4427e63b4706f7e3e1ec59ffc71c
Ssali Lwanga <ssalilwanga7@gmail.com>
2026-08-05 14:15:06 +0300

    Order number rendering

--- js/utils.js
+++ js/utils.js
@@ -333,16 +333,9 @@ function generateOrderNumber(){
     const now = new Date();
 
     const random =
+        Math.floor(Math.random() * 9000) + 1000;
 
-        Math.floor(
-
-            Math.random()*9000
-
-        ) + 1000;
-
-    return
-
- `TK-${now.getFullYear()}${String(now.getMonth()+1).padStart(2,"0")}${String(now.getDate()).padStart(2,"0")}-${random}`;
+    return `TK-${now.getFullYear()}${String(now.getMonth()+1).padStart(2,"0")}${String(now.getDate()).padStart(2,"0")}-${random}`;
 
 }
 
